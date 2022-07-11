8c5a6f49eee255715d56cc8320ae5a8aff44b8ce
tsubasa-alife <tsubasa.robotics@gmail.com>
2022-07-11 10:07:23 +0900

    Editor拡張の追加

---
+++ KelpNetSample/Assets/Editor/KelpNet/KelpNetTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using KelpNet;
+using KelpNet.CPU;
+
+
+public class KelpNetTest
+{
+    public static void TestFNN()
+    {
+
+    }
+    
+}
